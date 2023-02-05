ebaa39e6435d79903f01db18b77710bef3837b9a
Oki Kazuya <okik4zuya@gmail.com>
2023-02-05 20:21:57 +0700

    add excerpt display on lits page

--- src/pages/lits.js
+++ src/pages/lits.js
@@ -1,6 +1,5 @@
 import Loader from '@/components/Loader';
 import { useStore } from '@/store';
-import { useRouter } from 'next/router';
 import { useState } from 'react';
 import 'react-quill/dist/quill.snow.css';
 import { getUniqueValue, toExcerpt } from '@/functions';
@@ -154,48 +153,12 @@ const Lits = () => {
             </div>
             <div className='mt-2 md:w-1/2 w-full pb-20'>
                 {litslist.map((item, key) => (
-                    <div key={key} className="text-md font-semibold flex flex-col items-center bg-white rounded-lg shadow-md p-4 mb-2"
-                    >
-                        <div className='w-full text-lg font-bold'
-                        >
-                            {toExcerpt(item.title, 100)}
-                        </div>
-                        <div className='flex justify-end w-full mt-2 items-center'>
-                            <div className='flex'>
-                                <div className='py-1 px-4 rounded-lg text-sm text-white font-bold cursor-pointer'
-                                    style={{ background: "rgb(80, 104, 169)" }}
-                                    onClick={() => { setTab("Notes"); setSearchNote(item.title) }}
-                                >
-                                    {notes.filter(i => i.title == item.title).length} Notes
-                                </div>
-                            </div>
-                            <div className='flex flex-1 ml-2 items-center'>
-                                <a href={item.link}
-                                    target="_blank"
-                                    className="font-semibold"
-                                    style={{
-                                        pointerEvents: item.link ? "auto" : "none",
-                                        color: item.link ? "#5068a9" : "#e7e7e7",
-                                    }}
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                        <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                                        <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
-                                    </svg>
-                                </a>
-                            </div>
-                            <div onClick={() => openLitFormEdit(item.id)} className="cursor-pointer mr-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
-                                    <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
-                                </svg>
-                            </div>
-                            <div className='ml-2 cursor-pointer' onClick={() => deleteLit(item.id)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-                                    <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
-                                </svg>
-                            </div>
-                        </div>
+                    <div key={key}>
+                        <LitCard 
+                        data={item}
+                        openLitFormEdit={openLitFormEdit}
+                        deleteLit={deleteLit}
+                        />
                     </div>
                 ))}
 
@@ -206,3 +169,68 @@ const Lits = () => {
 
 }
 export default Lits;
+
+
+const LitCard = (props) => {
+    const {data, openLitFormEdit, deleteLit} = props;
+    const { notes, setSearchNote, setTab } = useStore(state => state);
+    const [isExcerpt, setIsExcerpt] = useState(true)
+    return (
+        <div className="text-md font-semibold flex flex-col items-center bg-white rounded-lg shadow-md p-4 mb-2"
+        >
+            <div className='w-full text-lg font-bold'
+            >
+                {isExcerpt ? toExcerpt(data.title, 100) : data.title}
+            </div>
+            <div className='flex justify-end w-full mt-2 items-center'>
+                <div className='flex'>
+                    <div className='py-1 px-4 rounded-lg text-sm text-white font-bold cursor-pointer'
+                        style={{ background: "rgb(80, 104, 169)" }}
+                        onClick={() => { setTab("Notes"); setSearchNote(data.title) }}
+                    >
+                        {notes.filter(i => i.title == data.title).length} Notes
+                    </div>
+                </div>
+                <div className='flex flex-1 ml-2 items-center'>
+                    <a href={data.link}
+                        target="_blank"
+                        className="font-semibold"
+                        style={{
+                            pointerEvents: data.link ? "auto" : "none",
+                            color: data.link ? "#5068a9" : "#e7e7e7",
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                            <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                        </svg>
+                    </a>
+                </div>
+                <div className='cursor-pointer mr-2' onClick={() => setIsExcerpt(!isExcerpt)}>
+                    {isExcerpt ?
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+                        </svg>
+
+                        :
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+                        </svg>
+                    }
+
+                </div>
+                <div onClick={() => openLitFormEdit(data.id)} className="cursor-pointer mr-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                        <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                    </svg>
+                </div>
+                <div className='ml-2 cursor-pointer' onClick={() => deleteLit(data.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                        <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    )
+}
